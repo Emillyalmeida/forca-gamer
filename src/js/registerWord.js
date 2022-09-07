20f7a2a78df8fc138f064287323ccfc43ec33028
emillyalmeida <emillyalmeidan@gmail.com>
2022-09-07 13:05:25 -0300

    add functions to regsiter word, and add media queries in the page

--- src/js/registerWord.js
+++ src/js/registerWord.js
@@ -1,7 +1,21 @@
 import Controller from "../controller/controller.js";
 
 const btnBack = document.getElementById("back");
+const save = document.getElementById("save");
+const texrArea = document.querySelector("textarea");
+const btnClose = document.getElementById("close-info");
+const btnColorMode = document.querySelector("header button");
 
 btnBack.addEventListener("click", () => {
-  window.location = "./src/pages/start.html";
+  window.location = "./start.html";
 });
+
+save.addEventListener("click", () => {
+  Controller.registerWord(texrArea.value);
+});
+
+btnClose.addEventListener("click", () => {
+  Controller.closeModal();
+});
+
+btnColorMode.addEventListener("click", Controller.changeTheme);
