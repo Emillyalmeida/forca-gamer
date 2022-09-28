61638532f66491b71cecc7b6324d98569b73187d
emillyalmeida <emillyalmeidan@gmail.com>
2022-09-28 00:07:14 -0300

    script: init logic to page game

--- src/js/game.js
+++ src/js/game.js
@@ -1,10 +1,53 @@
 import Controller from "../controller/controller.js";
+const word = Controller.sortWord();
 
 const btnColorMode = document.querySelector("header button");
 const main = document.querySelector("main");
+const tela = document.querySelector("canvas");
+const tablero = document.getElementById("forca").getContext("2d");
+const divLines = document.getElementById("lines-word");
+const keyboard = document.getElementById("keyboard");
+const divLetterIcorrect = document.getElementById("letters-incorrects");
+
+let listLettersIcorrect = [];
+let sizeWord = word.length;
+let acertos = 0;
 
 btnColorMode.addEventListener("click", Controller.changeTheme);
 
-const word = Controller.sortWord();
+console.log(word.split(""));
 
-main.innerHTML = `<h1>${word}</h1>`;
+word.split("").forEach((letter, i) => {
+  const divLetter = document.createElement("div");
+  divLetter.id = i;
+  divLetter.classList.add("line-letter");
+
+  divLines.appendChild(divLetter);
+});
+
+const showIncorrectLetter = () => {
+  divLetterIcorrect.innerHTML = "";
+  listLettersIcorrect.forEach((letter) => {
+    const spanLetter = document.createElement("span");
+    spanLetter.innerText = letter;
+
+    divLetterIcorrect.appendChild(spanLetter);
+  });
+};
+
+const getLetter = (letter) => {
+  const hasLetter = word.split("").filter((char) => char == letter);
+
+  if (hasLetter.length) {
+    acertos += hasLetter.length;
+  } else {
+    listLettersIcorrect.push(letter);
+    showIncorrectLetter();
+  }
+};
+
+for (let i = 0; i < keyboard.children.length; i++) {
+  let item = keyboard.children[i].addEventListener("click", (e) => {
+    getLetter(e.target.id);
+  });
+}
