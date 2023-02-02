c60a6796bfa6607d19c37a64b9d2a85a6ce8fb45
Emillyalmeida <emillyalmeidan@gmail.com>
2023-02-01 23:38:07 -0300

    style and script: add info user

--- src/js/start.js
+++ src/js/start.js
@@ -1,9 +1,23 @@
 import Controller from "../controller/controller.js";
 
+Controller.checkPlayer()
+
 const main = document.querySelector("main");
 const player = JSON.parse(localStorage.getItem("@forcaGame"));
 
-main.innerHTML = `<h2>Jogador: ${player._nome}</h2>
+main.innerHTML = `    <h2>Jogador: ${player._nome}</h2>
+                      <div class="content-info">
+                        <div class="info-player">
+                          <i class="fa-sharp fa-solid fa-trophy"></i>
+                          <h4>Vitórias</h4>
+                          <span>${player._vitorias}</span>
+                        </div>
+                        <div class="info-player">
+                          <i class="fa-solid fa-heart-crack"></i>
+                          <h4>Derrotas</h4>
+                          <span>${player._derrotas}</span>
+                        </div>
+                      </div>
                       <div class="div-buttons">
                         <button id="start">Começar a jogar</button>
                         <button id="newWord">Adicionar nova palavra</button>
